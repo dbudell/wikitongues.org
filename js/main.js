48ce2fa9873c4f5269a8b35a14585cde2eb6a5b5
Freddie Andrade <faandrade@gmail.com>
2014-03-15 23:23:24 -0400

    jquery header autoresize

--- js/main.js
+++ js/main.js
@@ -15,17 +15,8 @@ $(function(){
 				    }
 				})
 
-	function contactResize () {
-		var screenHeight = $(window).innerHeight(),
-		modalHeight = screenHeight - (246)
-		mobileModalHeight = screenHeight - (246)
-
-		if($(window).width() > 480) {
-				$('#overlay #contact-form #message').css('height',modalHeight)
-			} else {
-				$('#overlay #contact-form #message').css('height',mobileModalHeight)
-			}
-	}
+	// Logotype resize
+	$(".logotype").fitText(.712)
 
 	$(window).resize(function(){
 		contactResize()
