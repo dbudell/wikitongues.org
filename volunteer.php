86265058fd63aa307ab56df6fe79bcc4e7195e44
Daniel Bogre Udell <udell.db@gmail.com>
2014-07-23 21:53:46 -0400

    fixed array on master

--- volunteer.php
+++ volunteer.php
@@ -2,7 +2,9 @@
 
 $name = $_POST['name'];
 $email = $_POST['email'];
-$role = $_POST['check_list'];
+$role = "";
+// how to display commas to appear after all returned array items except the last?
+foreach ($_POST['check_list'] AS $checked_role) {$role .= " $checked_role".",";}
 $message = $_POST['message'];
 $formcontent="From: $name \n Desired Role: $role \n Message: $message";
 $recipient = "hello@wikitongues.org";
@@ -11,7 +13,8 @@ $mailheader = "From: $email \r\n";
 
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 header('Location: index.html');
+// how to append an html window confirming to the user that his or her application has been sent?
+//echo "<p>Thanks! We'll get back to you soon. We Promise.</p>";
 exit();
-//echo "Thanks! We'll get back to you soon. We Promise.";
 
 ?>
