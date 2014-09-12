53a18eae57560a591b7cc8209a8c22f9d132f6ab
Freddie Andrade <faandrade@gmail.com>
2014-09-12 13:53:42 -0400

    language list example page

--- js/test.js
+++ js/test.js
@@ -1,7 +1,15 @@
 $(window).load(function(){
-  var listLength = 6909
-  for (var i = 0; i < listLength; i++) {
-    console.log(i)
-    $(".list").append('<li>Language # '+i+'</li>')
+  var listLength = 6909,
+  recordedLength = 107,
+  recordedPercentage = Math.round(recordedLength/listLength*100)
+
+  for (var i = 1; i <= listLength; i++) {
+    $(".list").append('<li>Index '+i+' - Language Name<input type="checkbox" /></li>')
   }
+
+  $("p span.recorded-number").text(recordedLength)
+  $("p span.language-number").text(listLength)
+  $("p span.percentage").text(recordedPercentage+'%')
+
+  $(".progress-bar").css("width", recordedPercentage+'%')
 })
